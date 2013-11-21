bd571a03d854844f87179f436d5ccbb44a0594d8
Arijit Chattopahdyay <arijitnit06@gmail.com>
2013-11-21 17:30:41 -0500

    Adding Inliner.cpp

--- arijit/inliner.cpp
+++ arijit/inliner.cpp
@@ -6,6 +6,7 @@
 #include "llvm/CallingConv.h"
 #include "llvm/Support/CallSite.h"
 #include "llvm/Transforms/IPO/InlinerPass.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
@@ -15,20 +16,33 @@ namespace {
 			static char ID;
 			InlineCostAnalyzer *ICA;
 			MyInliner():Inliner(ID),ICA(0){} 	
-  			virtual InlineCost getInlineCost(CallSite CS);
+			virtual InlineCost getInlineCost(CallSite CS);
 			virtual bool runOnSCC(CallGraphSCC &scc);
+			virtual void getAnalysisUsage(AnalysisUsage &AN);
 	};
-}
 
-InlineCost MyInliner::getInlineCost(CallSite cs) {
-	errs()<<"Inline Cost Threshold is : "<<getInlineThreshold(cs)<<"\n";
-	return ICA->getInlineCost(cs,getInlineThreshold(cs));
-}
 
-bool MyInliner::runOnSCC(CallGraphSCC &scc) {
-	return false;
-}
+	InlineCost MyInliner::getInlineCost(CallSite cs) {
+		errs()<<"Inline Cost Threshold is : "<<getInlineThreshold(cs)<<"\n";
+		CallInst *callInst = reinterpret_cast<CallInst*>(cs.getInstruction());
+		Function *callee = cs.getCalledFunction();
+		errs()<<"Caller  function " <<callee->getName()<<"\n";
+		return ICA->getInlineCost(cs,getInlineThreshold(cs));
+	}
 
+	bool MyInliner::runOnSCC(CallGraphSCC &scc) {
+		ICA = &getAnalysis<InlineCostAnalyzer>();
+		return Inliner::runOnSCC(scc);
+		//  return false;
+		//
+	}
+
+	void MyInliner::getAnalysisUsage(AnalysisUsage &AU) {
+		AU.addRequired<InlineCostAnalyzer>() ;
+		Inliner::getAnalysisUsage(AU);
+	}
+
+}
 
 char MyInliner::ID = 0;
 static RegisterPass<MyInliner> X("ml","This is a custom inliner",false,false);
