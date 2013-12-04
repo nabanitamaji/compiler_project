55df535efb36f6297a84e085cc94424fd03ceed5
Arijit Chattopadhyay <arijitnit06@gmail.com>
2013-12-04 01:08:44 -0500

    Adding the new working profiler. -- Arijit.

--- profilellvm.c
+++ profilellvm.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
-#include<string.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 #include <stdlib.h>
 
-typedef struct edge{
+typedef struct Edge{
 	char * parent;
 	char * child;
 	int visited;
-	struct edge *next;
-} edge;
-static edge * list=NULL;
+	struct Edge *next;
+} Edge;
+static Edge * list=NULL;
 
 void profile_dump(){
 	const char* filename="tracedump.log";
 	FILE *file=fopen(filename,"w");
-	edge *node=list;
+	Edge *node=list;
 	if(file){
 		while(node){
 			fprintf(file,"\n%s %s %d\n",node->parent,node->child,node->visited);
@@ -24,24 +24,23 @@ void profile_dump(){
 	}	
 }
 
-void gen_profile(const void *s1, const void *s2)
-{
+void gen_profile(const void *s1, const void *s2) {
 	const char *from=(char *)s1;
 	const char *to=(char *)s2;
-	edge * node,*last;
+	Edge *node,*last;
+
 	if(list==NULL)
 	{
-		list=(edge *)malloc(sizeof(edge));
-		node=list;
+		node = (Edge *)malloc(sizeof(Edge));
 		node->parent=(char *)malloc(sizeof(char)*strlen(from));
 		strcpy(node->parent,from);
 		node->child=(char *)malloc(sizeof(char)*strlen(to));
 		strcpy(node->child,to);		
 		node->visited=1;
 		node->next=NULL;
+		list = node;
 		atexit(profile_dump);
-	}
-	else{
+	}else{
 		node=list;
 		int found=0;
 		while(node){
@@ -52,19 +51,61 @@ void gen_profile(const void *s1, const void *s2)
 			last=node;
 			node=node->next;
 		}
-		if(found==1)
-			(node->visited)++;
-		else{
-			last->next=(edge *)malloc(sizeof(edge));
-			node=last->next;
-			node->parent=(char *)malloc(sizeof(char)*strlen(from));
-	                strcpy(node->parent,from);
-        	        node->child=(char *)malloc(sizeof(char)*strlen(to));
-                	strcpy(node->child,to);
-                	node->visited=1;
-                	node->next=NULL;
+                if(found ==1){
+			node->visited++;
+		}else {
+			node = (Edge*) malloc(sizeof(Edge));
+			node->next = NULL;
+			node->parent = (char *)malloc(sizeof(char)*(strlen(from)+1));
+			strcpy(node->parent,from);
+			node->child = (char *) malloc(sizeof(char)*(strlen(to)+1));
+			strcpy(node->child,to);
+			node->visited = 1;
+			last->next = node;
 		}
-		
 	}
- //       profile_dump();
 }
+
+//void gen_profile(const void *s1, const void *s2)
+//{
+//	const char *from=(char *)s1;
+//	const char *to=(char *)s2;
+//	Edge *node,*last;
+//
+//	if(list==NULL)
+//	{
+//		list=(Edge *)malloc(sizeof(Edge));
+//		node=list;
+//		node->parent=(char *)malloc(sizeof(char)*strlen(from));
+//		strcpy(node->parent,from);
+//		node->child=(char *)malloc(sizeof(char)*strlen(to));
+//		strcpy(node->child,to);		
+//		node->visited=1;
+//		node->next=NULL;
+//		atexit(profile_dump);
+//	}else{
+//		node=list;
+//		int found=0;
+//		while(node){
+//			if(strcmp(node->parent,from)==0 && strcmp(node->child,to)==0){
+//				found=1;
+//				break;
+//			}
+//			last=node;
+//			node=node->next;
+//		}
+//		if(found==1)
+//			(node->visited)++;
+//		else{
+//			last->next=(Edge *)malloc(sizeof(Edge));
+//			node=last->next;
+//			node->parent=(char *)malloc(sizeof(char)*strlen(from));
+//	                strcpy(node->parent,from);
+//        	        node->child=(char *)malloc(sizeof(char)*strlen(to));
+//                	strcpy(node->child,to);
+//                	node->visited=1;
+//                	node->next=NULL;
+//		}
+//		
+//	}
+//}
