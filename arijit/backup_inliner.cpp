2362705a57b5dab5e552b47b6a8f18d52ed1a9f4
Arijit Chattopadhyay <arijitnit06@gmail.com>
2013-12-04 21:13:17 -0500

    Applying filter and some CallAnalyzer Code.--Arijit

--- arijit/backup_inliner.cpp
+++ arijit/backup_inliner.cpp
@@ -94,7 +94,7 @@ namespace {
 		int instrCount = getInstrCount(cs.getCalledFunction());
 		errs() <<" Instruction count  : "<<instrCount<<"\n";
 		
-		if(frq > 30  && instrCount < 50 ) {
+		if(frq > 30  && size < 30 ) {
 			errs()<<"Function being called more than 30 : "<<callerName<<"  :  "<<calleeName<<" Freq : "<<frq<<" Size : "<<size<<"\n";
 			return InlineCost::getAlways();
 		}
@@ -106,7 +106,7 @@ namespace {
 		ICA = &getAnalysis<InlineCostAnalysis>();
 		const TargetTransformInfo &TTI = getAnalysis<TargetTransformInfo>();
 		const DataLayout *TD = getAnalysisIfAvailable<DataLayout>();
-		CallAnalyzer CA (TD,*TTI,callee,225);
+		CallAnalyzer CA (
 		return Inliner::runOnSCC(scc);
 	}
 
@@ -115,6 +115,91 @@ namespace {
 		AU.addRequired<TargetTransformInfo>();
 		Inliner::getAnalysisUsage(AU);
 	}
+       // extern int getcount(const char*filename,const char* from,const char *to);
+
+       // InlineCost MyInliner::getInlineCost(CallSite cs) {
+
+       // 	return InlineCost::getAlways();		
+       // }
+	
+       // InlineCost MyInliner::getInlineCost(CallSite cs) {
+       // 
+       //         const char *callerName=  cs.getCaller()->getName().data();
+       //         const char *calleeName=  cs.getCalledFunction()->getName().data();
+
+       // 	errs()<<"Count is  : "<<freq.getFreq(callerName,calleeName)<<"\n";
+       //         InlineCost iCost1 = alwaysInline(cs);
+       //         InlineCost iCost2 = ICA->getInlineCost(cs,getInlineThreshold(cs));
+       //         errs() <<"The value of the icost1 : "<<iCost1.isAlways()<<"\n";
+       //         errs() <<"The value of the icost2 : "<<iCost2.isAlways()<<"\n";
+       // 	if(iCost1.isAlways() && iCost2.isAlways()) {
+       // 		return iCost1;
+       // 	} else {
+       // 		return iCost2;
+       // 	}
+       // 	
+       // 	/**
+       // 	 * This function is the one written in always inline function
+       // 	 */
+       // 	
+
+       //// 	Function *callee = cs.getCalledFunction();
+
+       //// 	//CallAnalyzer's Analyze call actuall determines to the major extent
+       //// 	// We will not do inlinig.
+       //// 	
+       //// 	//Do not inline if the callee does not exists
+       //// 	if(!callee) {
+       //// 		return InlineCost::getNever();
+       //// 	}
+       ////         errs()<<"Always inlining"<<"\n";
+       //// 	return InlineCost::getAlways();
+       ////        
+       //// 	//Do inline if the inline keyword is present
+       //// 	if(callee->hasFnAttribute(Attribute::AlwaysInline)) {
+       //// 		if(ICA->isInlineViable(*callee)) {
+       //// 			return InlineCost::getAlways();
+       //// 		}
+       //// 		return InlineCost::getNever();
+       //// 	}
+
+       //// 	//Do not Inline if recursive
+       //// 	bool isRecursive= false;
+       //// 	Function *caller = cs.getCaller();
+       //// 	for(Value::use_iterator  U = caller->use_begin(), E = caller->use_end();U != E ; ++U) {
+       //// 		CallSite site(cast<Value>(*U)) ;
+       //// 		if(!site)
+       //// 			continue;
+       //// 		Instruction *inst = site.getInstruction();
+       //// 		if(inst->getParent()->getParent() == caller) {
+       //// 			isRecursive = true;
+       //// 			break;
+       //// 		}
+       //// 	}
+
+       //// 	if(isRecursive) {
+       //// 		return InlineCost::getNever();	
+       //// 	}
+
+       //// 	//Get the information from the call Analyzer
+       //// 	
+       //// 	return InlineCost::getAlways();
+       //// 		       
+       //// 	/*
+       //// 	StringRef f("f1");
+       //// 	errs()<<"Caller  function " <<callee->getName()<<"\n";
+       //// 	if(callee->getName() == f) {
+       //// 	//return ICA->getInlineCost(cs,getInlineThreshold(cs));
+       //// 		return InlineCost::getAlways();
+       //// 	}
+       //// 	return InlineCost::getNever();
+
+       ////         */
+       // 	
+       // }
+
+
+
 
 }
 
