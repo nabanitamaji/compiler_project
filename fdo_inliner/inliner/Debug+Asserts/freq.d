33ed888cc5fee269f3ce1a18689a9e09898b1f1b
nabanita <nabanita@nabanita-VirtualBox.(none)>
2013-12-08 00:19:21 -0500

    adding fdo_inliner

---
+++ fdo_inliner/inliner/Debug+Asserts/freq.d
@@ -0,0 +1,5 @@
+/home/nabanita/llvm/projects/compiler_project/fdo_inliner/inliner/Debug+Asserts/freq.o \
+ /home/nabanita/llvm/projects/compiler_project/fdo_inliner/inliner/Debug+Asserts/freq.d: \
+ freq.cpp freq.h
+
+freq.h:
---
+++ fdo_inliner/inliner/Debug+Asserts/inliner.d
@@ -0,0 +1,229 @@
+/home/nabanita/llvm/projects/compiler_project/fdo_inliner/inliner/Debug+Asserts/inliner.o \
+ /home/nabanita/llvm/projects/compiler_project/fdo_inliner/inliner/Debug+Asserts/inliner.d: \
+ inliner.cpp /home/nabanita/llvm/include/llvm/Pass.h \
+ /home/nabanita/llvm/include/llvm/Support/Compiler.h \
+ /home/nabanita/llvm/include/llvm/Config/llvm-config.h \
+ /home/nabanita/llvm/include/llvm/PassSupport.h \
+ /home/nabanita/llvm/include/llvm/Pass.h \
+ /home/nabanita/llvm/include/llvm/InitializePasses.h \
+ /home/nabanita/llvm/include/llvm/PassRegistry.h \
+ /home/nabanita/llvm/include/llvm/ADT/StringRef.h \
+ /home/nabanita/llvm/include/llvm/Support/type_traits.h \
+ /home/nabanita/llvm/include/llvm/Support/DataTypes.h \
+ /home/nabanita/llvm/include/llvm/Support/CBindingWrapping.h \
+ /home/nabanita/llvm/include/llvm/Support/Casting.h \
+ /home/nabanita/llvm/include/llvm-c/Core.h \
+ /home/nabanita/llvm/include/llvm/Support/Atomic.h \
+ /home/nabanita/llvm/include/llvm/Support/Valgrind.h \
+ /home/nabanita/llvm/include/llvm/PassAnalysisSupport.h \
+ /home/nabanita/llvm/include/llvm/ADT/SmallVector.h \
+ /home/nabanita/llvm/include/llvm/Support/AlignOf.h \
+ /home/nabanita/llvm/include/llvm/Support/MathExtras.h \
+ /home/nabanita/llvm/include/llvm/Support/SwapByteOrder.h \
+ /home/nabanita/llvm/include/llvm/IR/Function.h \
+ /home/nabanita/llvm/include/llvm/IR/Argument.h \
+ /home/nabanita/llvm/include/llvm/ADT/Twine.h \
+ /home/nabanita/llvm/include/llvm/Support/ErrorHandling.h \
+ /home/nabanita/llvm/include/llvm/ADT/ilist_node.h \
+ /home/nabanita/llvm/include/llvm/IR/Attributes.h \
+ /home/nabanita/llvm/include/llvm/ADT/ArrayRef.h \
+ /home/nabanita/llvm/include/llvm/ADT/None.h \
+ /home/nabanita/llvm/include/llvm/ADT/FoldingSet.h \
+ /home/nabanita/llvm/include/llvm/Support/PointerLikeTypeTraits.h \
+ /home/nabanita/llvm/include/llvm/IR/Value.h \
+ /home/nabanita/llvm/include/llvm/IR/Use.h \
+ /home/nabanita/llvm/include/llvm/ADT/PointerIntPair.h \
+ /home/nabanita/llvm/include/llvm/IR/BasicBlock.h \
+ /home/nabanita/llvm/include/llvm/ADT/ilist.h \
+ /home/nabanita/llvm/include/llvm/IR/Instruction.h \
+ /home/nabanita/llvm/include/llvm/IR/User.h \
+ /home/nabanita/llvm/include/llvm/Support/DebugLoc.h \
+ /home/nabanita/llvm/include/llvm/IR/Instruction.def \
+ /home/nabanita/llvm/include/llvm/IR/SymbolTableListTraits.h \
+ /home/nabanita/llvm/include/llvm/IR/CallingConv.h \
+ /home/nabanita/llvm/include/llvm/IR/GlobalValue.h \
+ /home/nabanita/llvm/include/llvm/IR/Constant.h \
+ /home/nabanita/llvm/include/llvm/IR/DerivedTypes.h \
+ /home/nabanita/llvm/include/llvm/IR/Type.h \
+ /home/nabanita/llvm/include/llvm/ADT/APFloat.h \
+ /home/nabanita/llvm/include/llvm/ADT/APInt.h \
+ /home/nabanita/llvm/include/llvm/IR/Module.h \
+ /home/nabanita/llvm/include/llvm/ADT/OwningPtr.h \
+ /home/nabanita/llvm/include/llvm/IR/GlobalAlias.h \
+ /home/nabanita/llvm/include/llvm/IR/OperandTraits.h \
+ /home/nabanita/llvm/include/llvm/IR/GlobalVariable.h \
+ /home/nabanita/llvm/include/llvm/IR/Metadata.h \
+ /home/nabanita/llvm/include/llvm/Analysis/CallGraph.h \
+ /home/nabanita/llvm/include/llvm/ADT/GraphTraits.h \
+ /home/nabanita/llvm/include/llvm/ADT/STLExtras.h \
+ /home/nabanita/llvm/include/llvm/Support/CallSite.h \
+ /home/nabanita/llvm/include/llvm/IR/Instructions.h \
+ /home/nabanita/llvm/include/llvm/IR/InstrTypes.h \
+ /home/nabanita/llvm/include/llvm/Support/IncludeFile.h \
+ /home/nabanita/llvm/include/llvm/Support/ValueHandle.h \
+ /home/nabanita/llvm/include/llvm/ADT/DenseMapInfo.h \
+ /home/nabanita/llvm/include/llvm/Analysis/InlineCost.h \
+ /home/nabanita/llvm/include/llvm/Analysis/CallGraphSCCPass.h \
+ /home/nabanita/llvm/include/llvm/Transforms/IPO/InlinerPass.h \
+ /home/nabanita/llvm/include/llvm/Support/raw_ostream.h \
+ /home/nabanita/llvm/include/llvm/Support/FileSystem.h \
+ /home/nabanita/llvm/include/llvm/ADT/IntrusiveRefCntPtr.h \
+ /home/nabanita/llvm/include/llvm/ADT/SmallString.h \
+ /home/nabanita/llvm/include/llvm/Support/TimeValue.h \
+ /home/nabanita/llvm/include/llvm/Support/system_error.h \
+ /home/nabanita/llvm/include/llvm/Support/InstIterator.h \
+ /home/nabanita/llvm/include/llvm/Support/CommandLine.h \
+ /home/nabanita/llvm/include/llvm/ADT/StringMap.h \
+ /home/nabanita/llvm/include/llvm/Support/Allocator.h freq.h
+
+/home/nabanita/llvm/include/llvm/Pass.h:
+
+/home/nabanita/llvm/include/llvm/Support/Compiler.h:
+
+/home/nabanita/llvm/include/llvm/Config/llvm-config.h:
+
+/home/nabanita/llvm/include/llvm/PassSupport.h:
+
+/home/nabanita/llvm/include/llvm/Pass.h:
+
+/home/nabanita/llvm/include/llvm/InitializePasses.h:
+
+/home/nabanita/llvm/include/llvm/PassRegistry.h:
+
+/home/nabanita/llvm/include/llvm/ADT/StringRef.h:
+
+/home/nabanita/llvm/include/llvm/Support/type_traits.h:
+
+/home/nabanita/llvm/include/llvm/Support/DataTypes.h:
+
+/home/nabanita/llvm/include/llvm/Support/CBindingWrapping.h:
+
+/home/nabanita/llvm/include/llvm/Support/Casting.h:
+
+/home/nabanita/llvm/include/llvm-c/Core.h:
+
+/home/nabanita/llvm/include/llvm/Support/Atomic.h:
+
+/home/nabanita/llvm/include/llvm/Support/Valgrind.h:
+
+/home/nabanita/llvm/include/llvm/PassAnalysisSupport.h:
+
+/home/nabanita/llvm/include/llvm/ADT/SmallVector.h:
+
+/home/nabanita/llvm/include/llvm/Support/AlignOf.h:
+
+/home/nabanita/llvm/include/llvm/Support/MathExtras.h:
+
+/home/nabanita/llvm/include/llvm/Support/SwapByteOrder.h:
+
+/home/nabanita/llvm/include/llvm/IR/Function.h:
+
+/home/nabanita/llvm/include/llvm/IR/Argument.h:
+
+/home/nabanita/llvm/include/llvm/ADT/Twine.h:
+
+/home/nabanita/llvm/include/llvm/Support/ErrorHandling.h:
+
+/home/nabanita/llvm/include/llvm/ADT/ilist_node.h:
+
+/home/nabanita/llvm/include/llvm/IR/Attributes.h:
+
+/home/nabanita/llvm/include/llvm/ADT/ArrayRef.h:
+
+/home/nabanita/llvm/include/llvm/ADT/None.h:
+
+/home/nabanita/llvm/include/llvm/ADT/FoldingSet.h:
+
+/home/nabanita/llvm/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/nabanita/llvm/include/llvm/IR/Value.h:
+
+/home/nabanita/llvm/include/llvm/IR/Use.h:
+
+/home/nabanita/llvm/include/llvm/ADT/PointerIntPair.h:
+
+/home/nabanita/llvm/include/llvm/IR/BasicBlock.h:
+
+/home/nabanita/llvm/include/llvm/ADT/ilist.h:
+
+/home/nabanita/llvm/include/llvm/IR/Instruction.h:
+
+/home/nabanita/llvm/include/llvm/IR/User.h:
+
+/home/nabanita/llvm/include/llvm/Support/DebugLoc.h:
+
+/home/nabanita/llvm/include/llvm/IR/Instruction.def:
+
+/home/nabanita/llvm/include/llvm/IR/SymbolTableListTraits.h:
+
+/home/nabanita/llvm/include/llvm/IR/CallingConv.h:
+
+/home/nabanita/llvm/include/llvm/IR/GlobalValue.h:
+
+/home/nabanita/llvm/include/llvm/IR/Constant.h:
+
+/home/nabanita/llvm/include/llvm/IR/DerivedTypes.h:
+
+/home/nabanita/llvm/include/llvm/IR/Type.h:
+
+/home/nabanita/llvm/include/llvm/ADT/APFloat.h:
+
+/home/nabanita/llvm/include/llvm/ADT/APInt.h:
+
+/home/nabanita/llvm/include/llvm/IR/Module.h:
+
+/home/nabanita/llvm/include/llvm/ADT/OwningPtr.h:
+
+/home/nabanita/llvm/include/llvm/IR/GlobalAlias.h:
+
+/home/nabanita/llvm/include/llvm/IR/OperandTraits.h:
+
+/home/nabanita/llvm/include/llvm/IR/GlobalVariable.h:
+
+/home/nabanita/llvm/include/llvm/IR/Metadata.h:
+
+/home/nabanita/llvm/include/llvm/Analysis/CallGraph.h:
+
+/home/nabanita/llvm/include/llvm/ADT/GraphTraits.h:
+
+/home/nabanita/llvm/include/llvm/ADT/STLExtras.h:
+
+/home/nabanita/llvm/include/llvm/Support/CallSite.h:
+
+/home/nabanita/llvm/include/llvm/IR/Instructions.h:
+
+/home/nabanita/llvm/include/llvm/IR/InstrTypes.h:
+
+/home/nabanita/llvm/include/llvm/Support/IncludeFile.h:
+
+/home/nabanita/llvm/include/llvm/Support/ValueHandle.h:
+
+/home/nabanita/llvm/include/llvm/ADT/DenseMapInfo.h:
+
+/home/nabanita/llvm/include/llvm/Analysis/InlineCost.h:
+
+/home/nabanita/llvm/include/llvm/Analysis/CallGraphSCCPass.h:
+
+/home/nabanita/llvm/include/llvm/Transforms/IPO/InlinerPass.h:
+
+/home/nabanita/llvm/include/llvm/Support/raw_ostream.h:
+
+/home/nabanita/llvm/include/llvm/Support/FileSystem.h:
+
+/home/nabanita/llvm/include/llvm/ADT/IntrusiveRefCntPtr.h:
+
+/home/nabanita/llvm/include/llvm/ADT/SmallString.h:
+
+/home/nabanita/llvm/include/llvm/Support/TimeValue.h:
+
+/home/nabanita/llvm/include/llvm/Support/system_error.h:
+
+/home/nabanita/llvm/include/llvm/Support/InstIterator.h:
+
+/home/nabanita/llvm/include/llvm/Support/CommandLine.h:
+
+/home/nabanita/llvm/include/llvm/ADT/StringMap.h:
+
+/home/nabanita/llvm/include/llvm/Support/Allocator.h:
+
+freq.h:
